b08979d241afebb54af8d4603bfccf6f916c6bb8
Hairul Lana <hairullana99@gmail.com>
2022-09-18 08:55:43 +0800

    admin models, migration, seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +22,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // create default admin
+        Admin::create([
+            'username' => 'admin',
+            'password' => password_hash('admin', PASSWORD_DEFAULT),
+        ]);
     }
 }
