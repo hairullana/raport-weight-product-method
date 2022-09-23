d67704634f17528356be0ab1c5c40c84fc512c97
Hairul Lana <hairullana99@gmail.com>
2022-09-23 08:33:36 +0800

    update dashboad link sidebar

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -141,20 +141,6 @@
                     </div>
                     </section>
                 </div>
-                <footer>
-                <div class="footer clearfix mb-0 text-muted">
-                    <div class="float-start">
-                        <p>
-                            2022 &copy; Universitas Ahmad Dalam
-                        </p>
-                    </div>
-                    <div class="float-end">
-                        <p>
-                            Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by Baiq Nikum Yulisasih (1800018282)</a>
-                        </p>
-                    </div>
-                </div>
-                </footer>
             </div>
         </section>
     </div>
--- resources/views/templates/admin.blade.php
+++ resources/views/templates/admin.blade.php
@@ -32,7 +32,7 @@
                 <ul class="menu">
                     <li class="sidebar-title">Menu</li>
                     <li class="sidebar-item @if ($active == 'dashboard') active @endif">
-                        <a href="index.html" class='sidebar-link'>
+                        <a href="{{ route('admin.index') }}" class='sidebar-link'>
                         <i class="bi bi-grid-fill"></i>
                         <span>Dashboard</span>
                         </a>
