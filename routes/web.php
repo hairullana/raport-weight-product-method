73f0df157a38415087b7a4571332320519ccd09c
Hairul Lana <hairullana99@gmail.com>
2022-09-28 21:33:12 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\GuruController;
 |
 */
 
-Route::get('/', function () {
+Route::('/', function () {
     if (Auth::guard('admin')->check()) return redirect()->route('admin.index');
     elseif (Auth::guard('guru')->check()) return redirect()->route('guru.index');
     else return redirect()->route('auth.login');
@@ -25,46 +25,46 @@ Route::get('/', function () {
 
 Route::group(['controller' => AuthController::class, 'prefix' => '/auth', 'as' => 'auth.'], function () {
     Route::group(['middleware' => 'guest:admin,guru'], function () {
-        Route::get('/login', 'login')->name('login');
+        Route::('/login', 'login')->name('login');
         Route::post('/login', 'loginAction')->name('login-action');
     });
     Route::group(['middleware' => 'auth:admin,guru'], function () {
-        Route::get('/logout', 'logout')->name('logout');
+        Route::('/logout', 'logout')->name('logout');
     });
 });
 
 Route::group(['controller' => AdminController::class, 'prefix' => '/admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/daftar-siswa', 'daftarSiswa')->name('daftar-siswa');
-    Route::get('/daftar-siswa/tambah/{kelas}', 'tambahSiswa')->name('tambah-siswa');
+    Route::('/', 'index')->name('index');
+    Route::('/daftar-siswa', 'daftarSiswa')->name('daftar-siswa');
+    Route::('/daftar-siswa/tambah/{kelas}', 'tambahSiswa')->name('tambah-siswa');
     Route::post('/daftar-siswa/tambah/{kelas}', 'tambahSiswaAction')->name('tambah-siswa-action');
-    Route::get('/daftar-siswa/{kelas}', 'daftarSiswaDetail')->name('daftar-siswa-detail');
-    Route::get('/perhitungan', 'perhitungan')->name('perhitungan');
-    Route::get('/perhitungan/{kelas}', 'perhitunganDetail')->name('perhitungan-action');
-    Route::get('/nilai/update/{siswa_id}', 'updateNilai')->name('update-nilai');
+    Route::('/daftar-siswa/{kelas}', 'daftarSiswaDetail')->name('daftar-siswa-detail');
+    Route::('/perhitungan', 'perhitungan')->name('perhitungan');
+    Route::('/perhitungan/{kelas}', 'perhitunganDetail')->name('perhitungan-action');
+    Route::('/nilai/update/{siswa_id}', 'updateNilai')->name('update-nilai');
     Route::post('/nilai/update/{siswa_id}', 'updateNilaiAction')->name('update-nilai-action');
-    Route::get('/siswa-berprestasi', 'siswaBerprestasi')->name('siswa-berprestasi');
-    Route::get('/siswa-berprestasi/{kelas}', 'siswaBerprestasiDetail')->name('siswa-berprestasi-detail');
-    Route::get('/siswa/hapus/{siswa_id}', 'hapusSiswa')->name('hapus-siswa');
-    Route::get('/siswa/edit/{siswa_id}', 'editSiswa')->name('edit-siswa');
+    Route::('/siswa-berprestasi', 'siswaBerprestasi')->name('siswa-berprestasi');
+    Route::('/siswa-berprestasi/{kelas}', 'siswaBerprestasiDetail')->name('siswa-berprestasi-detail');
+    Route::('/siswa/hapus/{siswa_id}', 'hapusSiswa')->name('hapus-siswa');
+    Route::('/siswa/edit/{siswa_id}', 'editSiswa')->name('edit-siswa');
     Route::post('/siswa/edit/{siswa_id}', 'editSiswaAction')->name('edit-siswa-action');
-    Route::get('/siswa/status/{siswa_id}', 'updateSiswaStatus')->name('edit-siswa-status');
-    Route::get('/siswa/{siswa_id}/{juara?}', 'dataSiswa')->name('data-siswa');
-    Route::get('/users', 'users')->name('users');
-    Route::get('/users/{id}', 'editGuru')->name('edit-user');
+    Route::('/siswa/status/{siswa_id}', 'updateSiswaStatus')->name('edit-siswa-status');
+    Route::('/siswa/{siswa_id}/{juara?}', 'dataSiswa')->name('data-siswa');
+    Route::('/users', 'users')->name('users');
+    Route::('/users/{id}', 'editGuru')->name('edit-user');
     Route::post('/users/{id}', 'editGuruAction')->name('edit-user-action');
-    Route::get('/mode-lengkap/{kelas}', 'modeLengkap')->name('mode-lengkap');
+    Route::('/mode-lengkap/{kelas}', 'modeLengkap')->name('mode-lengkap');
 });
 
 Route::group(['controller' => GuruController::class, 'prefix' => '/guru', 'as' => 'guru.', 'middleware' => 'auth:guru'], function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/daftar-siswa', 'daftarSiswa')->name('daftar-siswa');
-    Route::get('/nilai/update/{siswa_id}', 'updateNilai')->name('update-nilai');
+    Route::('/', 'index')->name('index');
+    Route::('/daftar-siswa', 'daftarSiswa')->name('daftar-siswa');
+    Route::('/nilai/update/{siswa_id}', 'updateNilai')->name('update-nilai');
     Route::post('/nilai/update/{siswa_id}', 'updateNilaiAction')->name('update-nilai-action');
-    Route::get('/perhitungan', 'perhitungan')->name('perhitungan');
-    Route::get('/perhitungan/{kelas}', 'perhitunganDetail')->name('perhitungan-action');
-    Route::get('/siswa-berprestasi', 'siswaBerprestasi')->name('siswa-berprestasi');
-    Route::get('/siswa/{siswa_id}/{juara?}', 'dataSiswa')->name('data-siswa');
+    Route::('/perhitungan', 'perhitungan')->name('perhitungan');
+    Route::('/perhitungan/{kelas}', 'perhitunganDetail')->name('perhitungan-action');
+    Route::('/siswa-berprestasi', 'siswaBerprestasi')->name('siswa-berprestasi');
+    Route::('/siswa/{siswa_id}/{juara?}', 'dataSiswa')->name('data-siswa');
 });
 
-Route::get('/print/{kelas}', [AdminController::class, 'print'])->name('print')->middleware('auth:admin,guru');
+Route::('/print/{kelas}', [AdminController::class, 'print'])->name('print')->middleware('auth:admin,guru');
